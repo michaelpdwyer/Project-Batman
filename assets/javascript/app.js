51cf50a4d3301c85c5954d6fcc9d97aced6d06cc
katherine <kmarmitage@gmail.com>
2019-05-16 21:42:44 -0700

    commit working js with directions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,15 @@
 $(document).ready(function () {
     // Capture user inputs and store them into variables
-    // add a date format variable that handles trip date inputs
     var userLocation = '';
-    var tripDate = '';
-    var dateFormat = 'MM-DD-YYYY'
     var radius = 0; //search radius, in miles, starting from user location
     var difficulty = ''; //easy/moderate/challenging
     var hikeInfo = $("<div>");
 
-
+    //on click function for user search
     $("#submit").on("click", function (event) {
         // prevent page from refreshing when form tries to submit itself
         event.preventDefault();
-
+        //empty the div we will be appending to
         $("#tripDiv").empty();
 
         // Capture user inputs and store them in variables
@@ -21,15 +18,22 @@ $(document).ready(function () {
         radius = $("#radius").val().trim();
         difficulty = $("#difficulty").val().trim();
 
-        // Console log each of the user inputs to confirm we are receiving them
-        console.log(userLocation);
-        console.log(tripDate);
-        console.log(radius);
-        console.log(difficulty);
-
-        //this moment() js code captures the "trip date" field input (which is a string), stores it in 
-        //the format specified by the "dateFormat" variable and displays it in that format
-        // tripDate = moment(tripDate, dateFormat).format(dateFormat);
+        //variable to create a map for each difficulty input to the API color values
+        //this allows us to convert the six Hiker API difficulty tiers to our three tier difficulty input
+        var listDifficulty = {
+            'Green (Easy)': [
+                'green',
+                'greenBlue'
+            ],
+            'Medium': [
+                'blue',
+                'blueBlack'
+            ],
+            'Hard': [
+                'black',
+                'blackBlack'
+            ]
+        }
 
         axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
             params: {
@@ -44,7 +48,6 @@ $(document).ready(function () {
 
                 //now that we have the user's latitude & longitude, we can query
                 //the hiking project API 
-
                 // Constructing a URL to search the hiking project API using our given parameters
                 var apiKey = '200465587-99c82a449d86e59d5af9adda84b12cbb';
 
@@ -60,127 +63,76 @@ $(document).ready(function () {
                     .then(function (response) {
                         // Storing an array of results in the results variable
                         var results = response.trails;
-                        console.log(results);
+
                         //empty the hike info div
                         hikeInfo.empty();
 
                         //filter result set by difficulty
-                        if (difficulty === 'Green (Easy)') {
-                            for (var i = 0; i < results.length; i++) {
-                                // Store the hike's name & description, difficulty, location, and length
-                                if (results[i].difficulty === 'green' || results[i].difficulty === 'greenBlue') {
-                                    var name = results[i].name;
-                                    var nameDisplay = $("<p>").html("<strong>Hike name: </strong>" + name);
-                                    var location = results[i].location;
-                                    var locDisplay = $("<p>").html("<strong>Location:</strong> " + location);
-                                    var hikeDiff = results[i].difficulty;
-                                    var diffDisplay = $("<p>").html("<strong>Difficulty:</strong> Easy");
-                                    var length = results[i].length;
-                                    var lengthDisplay = $("<p>").html("<strong>Length in Miles:</strong> " + length);
-                                    var elevationGain = results[i].ascent;
-                                    var elevationDisplay = $("<p>").html("<strong>Elevation Gain:</strong> " + elevationGain + " ft");
-                                    var highestPoint = results[i].high;
-                                    var highestPointDisplay = $("<p>").html("<strong>Highest Point:</strong> " + highestPoint + " ft");
+                        for (var i = 0; i < results.length; i++) {
+                            // Store the hike's name & description, difficulty, location, and length
+                            if (results[i].difficulty === listDifficulty[difficulty][0] || results[i].difficulty === listDifficulty[difficulty][1]
+                            ) {
+                                //create a div to hold the hike info & add a class (which we can use for formatting)
+                                var hikeDiv = $("<div>");
+                                hikeDiv.addClass("info");
 
-                                    //Create an image tag for picture(s) of the hikes
-                                    var hikeImage = $("<img>");
-                                    // Giving the image tag an src attribute of a proprty from the result item
-                                    hikeImage.attr("src", results[i].imgSmallMed);
-                                    hikeImage.addClass("image");
-
-                                    // Appending the elements containing the hike info to the tripDiv
-                                    hikeInfo.append(nameDisplay, locDisplay, diffDisplay, lengthDisplay, elevationDisplay, highestPointDisplay, hikeImage);
-                                }
-                            }
-                        } else if (difficulty = 'Medium') {
-                            for (var i = 0; i < results.length; i++) {
-                                // Store the hike's name & description, difficulty, location, and length
-                                if (results[i].difficulty === 'blue' || results[i].difficulty === 'blueBlack') {
-                                    var name = results[i].name;
-                                    var nameDisplay = $("<p>").html("<strong>Hike name: </strong>" + name);
-                                    var location = results[i].location;
-                                    var locDisplay = $("<p>").html("<strong>Location:</strong> " + location);
-                                    var hikeDiff = results[i].difficulty;
-                                    var diffDisplay = $("<p>").html("<strong>Difficulty:</strong> Medium");
-                                    var length = results[i].length;
-                                    var lengthDisplay = $("<p>").html("<strong>Length in Miles:</strong> " + length);
-                                    var elevationGain = results[i].ascent;
-                                    var elevationDisplay = $("<p>").html("<strong>Elevation Gain:</strong> " + elevationGain + " ft");
-                                    var highestPoint = results[i].high;
-                                    var highestPointDisplay = $("<p>").html("<strong>Highest Point:</strong> " + highestPoint + " ft");
-
-                                    //Create an image tag for picture(s) of the hikes
-                                    var hikeImage = $("<img>");
-                                    // Giving the image tag an src attribute of a proprty from the result item
-                                    hikeImage.attr("src", results[i].imgSmallMed);
-                                    hikeImage.addClass("image");
-
-                                    // Appending the elements containing the hike info to the tripDiv
-                                    hikeInfo.append(nameDisplay, locDisplay, diffDisplay, lengthDisplay, elevationDisplay, highestPointDisplay, hikeImage);
-                                }
-                            }
-
-                        } else if (difficulty = 'Hard') {
-                            for (var i = 0; i < results.length; i++) {
-                                // Store the hike's name & description, difficulty, location, and length
-                                if (results[i].difficulty === 'black' || results[i].difficulty === 'blackBlack') {
-                                    var name = results[i].name;
-                                    var nameDisplay = $("<p>").html("<strong>Hike name: </strong>" + name);
-                                    var location = results[i].location;
-                                    var locDisplay = $("<p>").html("<strong>Location:</strong> " + location);
-                                    var hikeDiff = results[i].difficulty;
-                                    var diffDisplay = $("<p>").html("<strong>Difficulty:</strong> Hard");
-                                    var length = results[i].length;
-                                    var lengthDisplay = $("<p>").html("<strong>Length in Miles:</strong> " + length);
-                                    var elevationGain = results[i].ascent;
-                                    var elevationDisplay = $("<p>").html("<strong>Elevation Gain:</strong> " + elevationGain + " ft");
-                                    var highestPoint = results[i].high;
-                                    var highestPointDisplay = $("<p>").html("<strong>Highest Point:</strong> " + highestPoint + " ft");
-
-                                    //Create an image tag for picture(s) of the hikes
-                                    var hikeImage = $("<img>");
-                                    // Giving the image tag an src attribute of a proprty from the result item
-                                    hikeImage.attr("src", results[i].imgSmallMed);
-                                    hikeImage.addClass("image");
-
-                                    // Appending the elements containing the hike info to the tripDiv
-                                    hikeInfo.append(nameDisplay, locDisplay, diffDisplay, lengthDisplay, elevationDisplay, highestPointDisplay, hikeImage);
-                                }
-                            }
-
-                        } else {
-                            for (var i = 0; i < results.length; i++) {
-                                // Store the hike's name & description, difficulty, location, and length
+                                //assign API data to html elements
                                 var name = results[i].name;
-                                var nameDisplay = $("<p>").html("<strong>Hike name: </strong>" + name);
+                                var nameDisplay = $("<p class='resultText'>").html("<strong>Hike name: </strong>" + name);
                                 var location = results[i].location;
-                                var locDisplay = $("<p>").html("<strong>Location:</strong> " + location);
-                                var hikeDiff = results[i].difficulty;
-                                var diffDisplay = $("<p>").html("<strong>Difficulty:</strong> " + hikeDiff);
+                                var locDisplay = $("<p class='resultText'>").html("<strong>Location:</strong> " + location);
+                                var summary = results[i].summary;
+                                var sumDisplay = $("<p class='resultText'>").html("<strong>Summary:</strong> " + summary);
+                                var diffDisplay = $("<p class='resultText'>").html("<strong>Difficulty:</strong> " + difficulty);
                                 var length = results[i].length;
-                                var lengthDisplay = $("<p>").html("<strong>Length in Miles:</strong> " + length);
+                                var lengthDisplay = $("<p class='resultText'>").html("<strong>Length in Miles:</strong> " + length);
                                 var elevationGain = results[i].ascent;
-                                var elevationDisplay = $("<p>").html("<strong>Elevation Gain:</strong> " + elevationGain + " ft");
+                                var elevationDisplay = $("<p class='resultText'>").html("<strong>Elevation Gain:</strong> " + elevationGain + " ft");
                                 var highestPoint = results[i].high;
-                                var highestPointDisplay = $("<p>").html("<strong>Highest Point:</strong> " + highestPoint + " ft");
+                                var highestPointDisplay = $("<p class='resultText'>").html("<strong>Highest Point:</strong> " + highestPoint + " ft");
+
+                                //set latitude and longitude values for the hike location to variables
+                                var hikeLat = String(results[i].latitude);
+                                var hikeLng = String(results[i].longitude);
+                                //combine the lat and long variables, separated by a comma
+                                var hikeLocation = hikeLat + ',' + hikeLng;
+
+                                //create URL that accepts the userLocation and the hike location (in latitude/longitude)
+                                //we will use this to get Google Maps directions
+                                var mapsURL = "https://www.google.com/maps/dir/" + userLocation + "/" + hikeLocation;
+                                encodeURI(mapsURL);
+                                //create a form that will allow the user to click a button & get directions
+                                var uniqueForm = $("<form id='directionsForm'>");
+                                uniqueForm.attr("action", mapsURL);
+                                //create button
+                                var uniqueButton = $("<input id='directionsButton'>");
+                                uniqueButton.attr("type", "submit");
+                                uniqueButton.val("Get Directions");
+                                //append button to form
+                                uniqueForm.append(uniqueButton);
 
                                 //Create an image tag for picture(s) of the hikes
                                 var hikeImage = $("<img>");
-                                // Giving the image tag an src attribute of a proprty from the result item
+                                // Give the image tag a src attribute of the image property from the result item
                                 hikeImage.attr("src", results[i].imgSmallMed);
-                                hikeImage.addClass("image");
 
-                                // Appending the elements containing the hike info to the tripDiv
-                                hikeInfo.append(nameDisplay, locDisplay, diffDisplay, lengthDisplay, elevationDisplay, highestPointDisplay, hikeImage);
+                                //add image class to the hike image
+                                hikeImage.addClass("resultImage");
+                                // Appending the elements containing the hike info to the hikeDiv
+                                hikeDiv.append(hikeImage, nameDisplay, locDisplay, sumDisplay, diffDisplay, lengthDisplay, elevationDisplay,
+                                    highestPointDisplay, uniqueForm);
+
+                                //append hike div to hike info div
+                                hikeInfo.append(hikeDiv);
                             }
-
                         }
-                        // Prepending the hike info div to the "#tripDiv" div in the HTML
-                        $("#tripDiv").append(hikeInfo);
 
-                    });
+                    }) // Prepending the hike info div to the "#tripDiv" div in the HTML
+                $("#tripDiv").append(hikeInfo);
+
             });
-
     });
 
 });
+
+
